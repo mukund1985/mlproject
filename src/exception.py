6cd9a0c5017ba4ffe279cd434b1a5ba23fb32fca
Mukund Pandey <mukund.pandey@gmail.com>
2023-12-20 05:19:31 +0000

    updating code

--- src/exception.py
+++ src/exception.py
@@ -1,27 +1,46 @@
 import sys
-from logger import logger # Import the configured logger
+from logger import logger  # Import the configured logger from the logger module
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error))
+def error_message_detail(error, error_detail: sys):
+    # Extracting the traceback object from the error detail.
+    # 'error_detail' is expected to be a tuple containing exception type, value, and traceback.
+    _, _, exc_tb = error_detail
+
+    # Retrieving the file name and line number where the error occurred.
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    # Formatting the error message with the script name, line number, and error message.
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error))
 
     return error_message
 
 class CustomException(Exception):
+    # Custom exception class for more detailed error handling.
     def __init__(self, error_message, error_detail):
+        # Initializing the base Exception class with the error message.
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail)
+
+        # Storing a detailed error message, including file and line number of the error.
+        self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
+        # Overriding the string representation of the exception to return the detailed error message.
         return self.error_message
     
-if __name__=="__main__":
+# The following commented-out code demonstrates how to use the CustomException in an application.
+# Uncomment to use in a script where you need detailed error handling.
 
-    try:
-        a=1/0
-    except Exception as e:
-        exc_type, exc_value, exc_tb = sys.exc_info()
-        logger.info("Divide by Zero") # Use the imported logger
-        raise CustomException(e, (exc_type, exc_value, exc_tb))
+# if __name__ == "__main__":
+#     try:
+#         # Example code that might raise an exception.
+#         a = 1 / 0
+#     except Exception as e:
+#         # Capturing the exception details.
+#         exc_type, exc_value, exc_tb = sys.exc_info()
+
+#         # Logging the error using the imported logger.
+#         logger.info("Divide by Zero") 
+
+#         # Raising the custom exception with the original error and detailed information.
+#         raise CustomException(e, (exc_type, exc_value, exc_tb))
--- src/logger.py
+++ src/logger.py
@@ -2,24 +2,41 @@ import logging
 import os
 from datetime import datetime
 
-# Generating a unique log file name
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Generating a unique log file name using the current date and time.
+# This ensures that each log file is distinct and easily identifiable.
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-# Creating a directory path for logs
+# Creating a directory path for storing log files.
+# The logs directory is set to be inside the current working directory.
 logs_dir = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_dir, exist_ok=True)  # Ensure the directory exists
 
-# Defining the full log file path
+# Ensuring that the logs directory exists. If it doesn't, it's created.
+# 'exist_ok=True' prevents an error if the directory already exists.
+os.makedirs(logs_dir, exist_ok=True)
+
+# Defining the full path for the log file.
+# This combines the logs directory path with the generated log file name.
 LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
+# Setting up the basic configuration for the logging system.
+# 'filename' specifies the file where logs will be stored.
+# 'format' defines the structure of the log messages, including timestamp, line number, logger name, log level, and the message.
+# 'level' sets the threshold for logging. Messages that are less severe than this level will be ignored.
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 
-# Creating and exporting the logger object
-logger = logging.getLogger(__name__)
+# The following commented-out code is for creating a logger object and testing the logging setup.
+# Uncomment to use in an application where you need logging.
 
-if __name__=="__main__":
-    logging.info("Logging has started")
+# # Creating and exporting the logger object for use in other modules.
+# # '__name__' gives the logger the name of the current module.
+# logger = logging.getLogger(__name__)
+
+# # Example use of the logger in the main part of the application.
+# # This is a conditional that checks if the script is run as the main program.
+# if __name__ == "__main__":
+#     # Writing an info-level log message indicating that logging has started.
+#     logging.info("Logging has started")
